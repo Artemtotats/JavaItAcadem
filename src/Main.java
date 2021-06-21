d8f5a2912e5188643f7908a611f740eb9f41c602
Artemtotats <totot.art@yandex.com>
2021-06-21 14:34:35 +0300

    add 4

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,49 @@ public class Main {
     private static Object Solution;
 
     public static void main(String[] args) {
-         Metods metods =new Metods();
-         Metods.dayOffTheWeek();
 
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Вабери метод 1-10, (0-вызод из программы)");
+            switch (scanner.nextInt()) {
+                case 1:
+                    Methods.mean();
 
+                    break;
+                case 2:
+                    Methods.evan();
+                    break;
+                case 3:
+                    Methods.dayOffTheWeek();
+                    break;
+                case 4:
+                    Methods.random(7, 79);
+                    break;
+                case 5:
+                    Methods.sum(2);
+                    break;
+                case 6:
+                    Methods.whole();
+                    break;
+                case 7:
+                    Methods.fraction();
+                    break;
+                case 8:
+                    Methods.pow();
+                    break;
+                case 9:
+                    Methods.meanThree();
+                    break;
+                case 10:
+                    Methods.name();
+                    break;
+                default:
+                    System.out.println("Неверный ввод");
+                case 0:
+                    System.out.println("выход из программы");
+                    return;
+
+            }
+        }
     }
-
 }
--- src/Methods.java
+++ src/Methods.java
@@ -2,97 +2,116 @@ import java.util.Calendar;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Metods {
+public class Methods {
 
-    static void name() {
+
+    public static String name() {
         Scanner input = new Scanner(System.in);
 
         System.out.println("вводите именя пользователя с клавиатуры");
         String name = input.nextLine();
         System.out.println("именя пользователя: " + " " + name);
+        return name;
     }
 
-    static void whole() {
+    static int whole() {
         Scanner input = new Scanner(System.in);
         System.out.println("вводите целое число с клавиатуры ");
         int whole = input.nextInt();
+        return whole;
     }
 
-    static void fraction() {
+    static double fraction() {
         Scanner input = new Scanner(System.in);
         System.out.println("вводите дробное число с клавиатуры ");
         double fraction = input.nextDouble();
+        return fraction;
     }
 
-    static void pow() {
+    static int pow() {
         Scanner pow = new Scanner(System.in);
         System.out.println("введите число для возведения его в квадрат ");
         int value = pow.nextInt();
         System.out.println("квадрат числа равен" + " " + value * value);
-
+        return value;
     }
 
-    static void mean() {
+    static double mean() {
         Scanner mean = new Scanner(System.in);
         System.out.println("введите два числа для для  подсчета среднеарифметического ");
         double a = mean.nextInt();
         double b = mean.nextInt();
-        System.out.println("среднеарифметическое двух чисел авно" + " " + (a + b) / 2);
-
+        double mean1 = (a + b) / 2;
+        System.out.println("среднеарифметическое двух чисел авно" + " " + mean1);
+        return mean1;
     }
 
-    static void meanThree() {
+    static double meanThree() {
         Scanner meanThree = new Scanner(System.in);
         System.out.println("введите три числа для для  подсчета среднеарифметического ");
         double a = meanThree.nextInt();
         double b = meanThree.nextInt();
         double c = meanThree.nextInt();
-        System.out.println("среднеарифметическое трех чисел равно" + " " + (a + b + c) / 3);
+        double meanThrees = (a + b + c) / 3;
+        System.out.println("среднеарифметическое трех чисел равно" + " " + meanThrees);
+        return meanThrees;
     }
 
-    static void random() {
+    static int random(int min, int max) {
         Random random = new Random();
-        System.out.println("рандомное число равняется :" + " " + Math.random());
+        int randoms = (random.nextInt((max - min) + 1));
+        System.out.println("рандомное число равняется :" + " " + randoms);
+        return randoms;
     }
 
-    static void evan() {
+    public static String evan() {
         System.out.println("введите число для провеки ");
         Scanner evan = new Scanner(System.in);
         int a = evan.nextInt();
-        if (a % 2 == 0) {
-            System.out.println("Число четное");
-        } else {
-            System.out.println("Число не четное");
-        }
+        String evanOrNotEvan = a % 2 == 0 ? "Число четное" : "Число не четное";
+        System.out.println(evanOrNotEvan);
+        return evanOrNotEvan;
     }
 
-    static void dayOffTheWeek() {
+    static int dayOffTheWeek() {
         Calendar calendar = Calendar.getInstance();
         int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
         switch (dayOfWeek) {
             case 1:
-                System.out.println("день недели :" + " " + dayOfWeek + " "+"Воскресение");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Воскресение");
                 break;
             case 2:
-                System.out.println("день недели :" + " " + dayOfWeek + " "+"Понедельник");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Понедельник");
                 break;
             case 3:
-                System.out.println("день недели :" + " " + dayOfWeek + " "+"Вторник");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Вторник");
                 break;
             case 4:
-                System.out.println("день недели :" + " " + dayOfWeek +" "+ "Среда");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Среда");
                 break;
             case 5:
-                System.out.println("день недели :" + " " + dayOfWeek +" "+ "Четверг");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Четверг");
                 break;
             case 6:
-                System.out.println("день недели :" + " " + dayOfWeek +" "+ "Пятница");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Пятница");
                 break;
             case 7:
-                System.out.println("день недели :" + " " + dayOfWeek + " "+"Субота");
+                System.out.println("день недели :" + " " + dayOfWeek + " " + "Субота");
                 break;
 
 
         }
+        return dayOfWeek;
     }
+
+    public static int sum(int n) {
+        int sum = 0;
+        if (n <= 1) {
+            System.out.println("Введенное щначение должно быть больше 0");
+        }
+        for (int i = 0; i <= n; i++) sum = sum + i;
+        System.out.println("Сумма чисел равна:" + " " + sum);
+        return sum;
+    }
+
 }
