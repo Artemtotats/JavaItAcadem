06a174f1f4d5af4eec9ceff436ab0d2ea361c666
Artemtotats <totot.art@yandex.com>
2021-07-19 16:51:59 +0300

    task11

--- src/task11/Main.java
+++ src/task11/Main.java
@@ -2,10 +2,10 @@ package task11;
 
 public class Main {
     public static void main(String[] args) {
-        OnlineStore onlineStore= new OnlineStore( );
-        onlineStore.Registration("sam","12345");
-       // onlineStore.Authentication( );
+        OnlineStore onlineStore = new OnlineStore(" ", "12345");
+        onlineStore.Registration();
+        onlineStore.Authentication("sam", "12345");
     }
 
-    }
+}
 
--- src/task11/OnlineStore.java
+++ src/task11/OnlineStore.java
@@ -1,6 +1,8 @@
 package task11;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OnlineStore {
     private String login;
@@ -42,24 +44,33 @@ public class OnlineStore {
         this.inputPassword = inputPassword;
     }
 
-    public void Registration(String login, String password) {
+    public OnlineStore(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
 
-        if ( getPassword()==("\\D&\\d")) {
+    public void Registration() {
+        String stringPattern = "^.*(?=.{8,20})(?=..*[0-9])(?=.*[a-z])";
+        String nowPassword = getPassword();
+        Pattern pattern = Pattern.compile(stringPattern);
+        Matcher matcher = pattern.matcher(nowPassword);
+        while (matcher.find()) {
+            System.out.println(matcher.group());
             try {
                 throw new IOException();
             } catch (IOException e) {
                 e.printStackTrace();
-                System.err.println(" пароль должен содержать хотя ьа одну цифру и буквы латинского алфавита");
+                System.err.println(" пароль должен содержать хотя бы одну цифру и буквы латинского алфавита");
             }
-            this.login = login;
+
+
+
         }
-        this.password = password;
+
     }
 
 
-
-
-    public void  Authentication(String inputLogin, String inputPassword) {
+    public void Authentication(String inputLogin, String inputPassword) {
         if (!inputLogin.equals(getLogin()) || !inputPassword.equals(getPassword())) {
             try {
 
